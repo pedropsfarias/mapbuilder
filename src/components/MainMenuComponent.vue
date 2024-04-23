c311c85102f3a497003b95702a57116447a5e908
Pedro Farias <pedropsfarias@gmail.com>
2024-04-22 23:36:50 -0300

    Add basic layout

---
+++ src/components/MainMenuComponent.vue
@@ -0,0 +1,73 @@
+<template>
+  <Menubar :model="itens" start="search" />
+</template>
+
+<script>
+import InputText from 'primevue/inputtext';
+import Menubar from 'primevue/menubar';
+
+export default {
+  components: {
+    InputText,
+    Menubar
+  },
+  data() {
+    return {
+      search: null,
+      itens: [
+        {
+          label: 'Home',
+          icon: 'pi pi-home'
+        },
+        {
+          label: 'Features',
+          icon: 'pi pi-star'
+        },
+        {
+          label: 'Projects',
+          icon: 'pi pi-search',
+          items: [
+            {
+              label: 'Components',
+              icon: 'pi pi-bolt'
+            },
+            {
+              label: 'Blocks',
+              icon: 'pi pi-server'
+            },
+            {
+              label: 'UI Kit',
+              icon: 'pi pi-pencil'
+            },
+            {
+              label: 'Templates',
+              icon: 'pi pi-palette',
+              items: [
+                {
+                  label: 'Apollo',
+                  icon: 'pi pi-palette'
+                },
+                {
+                  label: 'Ultima',
+                  icon: 'pi pi-palette'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          label: 'Contact',
+          icon: 'pi pi-envelope'
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.p-menubar {
+  padding: 0.2rem;
+  border: none;
+}
+</style>
---
+++ src/components/MapComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="map"></div>
+</template>
+
+<script lang="ts">
+import maplibregl from 'maplibre-gl';
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  mounted() {
+    var map = new maplibregl.Map({
+      container: 'map', // container id
+      style: 'https://demotiles.maplibre.org/style.json', // style URL
+      center: [0, 0], // starting position [lng, lat]
+      zoom: 1 // starting zoom
+    });
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+#map {
+  height: 100%;
+  width: 100%;
+  background-color: grey;
+  border-radius: 0.5rem;
+  overflow: hidden;
+}
+</style>
