5c20651a62d87c6ccc31ab913b946b8d33aec265
Pedro Farias <pedropsfarias@gmail.com>
2024-04-26 17:57:51 -0300

    Try implement AppController

--- components.d.ts
+++ components.d.ts
@@ -7,14 +7,18 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    copy: typeof import('./src/components/MeasureComponent copy.vue')['default']
     InlineToolbarComponent: typeof import('./src/components/InlineToolbarComponent.vue')['default']
     InputText: typeof import('primevue/inputtext')['default']
+    LayerSwitcherComponent: typeof import('./src/components/LayerSwitcherComponent.vue')['default']
     MainMenuComponent: typeof import('./src/components/MainMenuComponent.vue')['default']
     MapComponent: typeof import('./src/components/MapComponent.vue')['default']
     MeasureComponent: typeof import('./src/components/MeasureComponent.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     SplitButton: typeof import('primevue/splitbutton')['default']
+    TabPanel: typeof import('primevue/tabpanel')['default']
+    TabView: typeof import('primevue/tabview')['default']
     Toolbar: typeof import('primevue/toolbar')['default']
   }
 }
---
+++ src/components/LayerSwitcherComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        LayerSwitcher Component
+        <br>
+        {{ message }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LayerSwitcherComponent',
+    data() {
+        return {
+            message: 'Hello from LayerSwitcherComponent'
+        }
+    }
+}
+</script>
+
+<style scoped></style>
