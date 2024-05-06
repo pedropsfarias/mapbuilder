264633ba51dd7af1efda85e8991abf1dc31f314d
Pedro Farias <pedropsfarias@gmail.com>
2024-05-06 15:25:33 -0300

    Many modifications

--- src/classes/map/Interaction.js
+++ src/classes/map/Interaction.js
@@ -1,6 +1,5 @@
 import MapSingleton from '../MapSingleton';
-import { distance, point, nearestPointOnLine, polygonToLine, pointToLineDistance, geomEach, flatten, segmentEach } from '@turf/turf'
-
+import { distance, point, nearestPointOnLine, pointToLineDistance, segmentEach } from '@turf/turf'
 
 class Interaction {
   constructor() {
@@ -11,9 +10,19 @@ class Interaction {
     this.snapedPoint = null;
     this.mousePoint = null;
 
+    this._createInternalLayer();
+  }
+
+  /**
+   * @private
+   * @memberof Interaction
+   * @description Create internal layer for snapping
+   * @returns {void}
+   */
+  _createInternalLayer() {
     this.map.addSource('__interaction', {
       'type': 'geojson',
-      'data': this.getEmptyDataSource()
+      'data': this._getEmptyDataSource()
     });
 
     this.map.addLayer({
@@ -30,15 +39,22 @@ class Interaction {
     });
   }
 
+  /**
+   * @private
+   * @memberof Interaction
+   * @description Snap to nearest point
+   * @param {Object} e
+   * @returns {void}
+   */
   _snap(e) {
     const zoom = this.map.getZoom();
     if (zoom < this.minZoom) {
-      this.map.getSource('__interaction').setData(this.getEmptyDataSource());
+      this.map.getSource('__interaction').setData(this._getEmptyDataSource());
       this.snapedPoint = null;
       return;
     }
 
-    var bbox = [
+    const bbox = [
       [e.point.x - this.tolerance * 3, e.point.y - this.tolerance * 3],
       [e.point.x + this.tolerance * 3, e.point.y + this.tolerance * 3]
     ];
@@ -46,7 +62,7 @@ class Interaction {
     const features = this.map.queryRenderedFeatures(bbox);
 
     if (!features.length) {
-      this.map.getSource('__interaction').setData(this.getEmptyDataSource());
+      this.map.getSource('__interaction').setData(this._getEmptyDataSource());
       this.snapedPoint = null;
       return;
     }
@@ -60,7 +76,7 @@ class Interaction {
         if (dist < this.tolerance) {
           this.map.getSource('__interaction').setData(feature);
         } else {
-          this.map.getSource('__interaction').setData(this.getEmptyDataSource());
+          this.map.getSource('__interaction').setData(this._getEmptyDataSource());
         }
 
       } else if (geom.type == 'MultiPolygon' || geom.type == 'Polygon') {
@@ -95,44 +111,76 @@ class Interaction {
     }
   }
 
-  _mouseMoveEvent(e) {
+  /**
+   * @private
+   * @memberof Interaction
+   * @description Mouse move event handler
+   * @param {Object} e
+   * @returns {void}
+   */
+  _mouseMoveEventHandler(e) {
     if (this.snapEnabled) {
       this._snap(e);
       return;
     }
-    this.mousePoint = point([e.lngLat.lng, e.lngLat.lat]);
   }
 
-  _mouseMove() {
-    this.map.on('mousemove', (e) => this._mouseMoveEvent(e));
+  /**
+   * @private
+   * @memberof Interaction
+   * @description Watch mouse move event
+   * @returns {void}
+   */
+  _watchMouseMove() {
+    this._mouseMoveFunction = (e) => this._mouseMoveEventHandler(e);
+    this.map.on('mousemove', this._mouseMoveFunction);
   }
 
-  getEmptyDataSource() {
+  /**
+   * @private
+   * @memberof Interaction
+   * @description Watch mouse click event
+   * @async
+   * @returns {Promise}
+   */
+  _watchMouseClick() {
+    return new Promise((resolve) => {
+      this.map.once('click', (e) => {
+        this.mousePoint = point([e.lngLat.lng, e.lngLat.lat]);
+        const clickPoint = this.snapedPoint || this.mousePoint;
+        this.map.off('mousemove', this._mouseMoveFunction);
+        this.map.getSource('__interaction').setData(this._getEmptyDataSource());
+        resolve(clickPoint);
+      });
+    });
+  }
+
+  /**
+   * @private
+   * @memberof Interaction
+   * @description Get empty data source
+   * @returns {Object} Empty data source
+   */
+  _getEmptyDataSource() {
     return {
       'type': 'FeatureCollection',
       'features': []
     }
   }
 
-  getDataSourcePoint({ lat, lng }) {
-    return {
-      'type': 'FeatureCollection',
-      'features': [
-        {
-          'type': 'Feature',
-          'geometry': {
-            'type': 'Point',
-            'coordinates': [lng, lat]
-          }
-        }
-      ]
-    }
-  }
-
-
-  getPoint() {
-
-    this._mouseMove();
+  /**
+   * Get point from map from mouse click
+   *
+   * @param {Boolean} snap - Snap to nearest point
+   * @memberof Interaction
+   * @returns {Object} Point
+   * @async
+   */
+  async getPoint(snap = true) {
+    this.snapEnabled = snap;
+    this._watchMouseMove();
+    const point = await this._watchMouseClick();
+    return point;
   }
 }
 
