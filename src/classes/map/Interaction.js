2f810def6de9d36919515be7250016a8206119c1
Pedro Farias <pedropsfarias@gmail.com>
2024-05-06 11:50:04 -0300

    Many modifications

--- src/classes/map/Interaction.js
+++ src/classes/map/Interaction.js
@@ -4,84 +4,114 @@ import { distance, point, nearestPointOnLine, polygonToLine, pointToLineDistance
 
 class Interaction {
   constructor() {
-    this.map = MapSingleton.getInstance().getMap()
-    this.tolerance = 3
-    this.isRunning = false;
+    this.map = MapSingleton.getInstance().getMap();
+    this.snapEnabled = true;
+    this.tolerance = 3;
+    this.minZoom = 16;
+    this.snapedPoint = null;
+    this.mousePoint = null;
+
+    this.map.addSource('__interaction', {
+      'type': 'geojson',
+      'data': this.getEmptyDataSource()
+    });
+
+    this.map.addLayer({
+      'id': '__interaction',
+      "type": "symbol",
+      'source': '__interaction',
+      "layout": {
+        "text-field": "×",
+        "text-size": 24,
+      },
+      "paint": {
+        "text-color": "magenta"
+      }
+    });
+  }
+
+  _snap(e) {
+    const zoom = this.map.getZoom();
+    if (zoom < this.minZoom) {
+      this.map.getSource('__interaction').setData(this.getEmptyDataSource());
+      this.snapedPoint = null;
+      return;
+    }
+
+    var bbox = [
+      [e.point.x - this.tolerance * 3, e.point.y - this.tolerance * 3],
+      [e.point.x + this.tolerance * 3, e.point.y + this.tolerance * 3]
+    ];
+    const mousePoint = point([e.lngLat.lng, e.lngLat.lat]);
+    const features = this.map.queryRenderedFeatures(bbox);
+
+    if (!features.length) {
+      this.map.getSource('__interaction').setData(this.getEmptyDataSource());
+      this.snapedPoint = null;
+      return;
+    }
+
+    for (let i = 0; i < features.length; i++) {
+      const feature = features[i];
+      const geom = feature.geometry;
+
+      if (geom.type === 'Point') {
+        const dist = distance(geom, mousePoint) * 1000.0;
+        if (dist < this.tolerance) {
+          this.map.getSource('__interaction').setData(feature);
+        } else {
+          this.map.getSource('__interaction').setData(this.getEmptyDataSource());
+        }
+
+      } else if (geom.type == 'MultiPolygon' || geom.type == 'Polygon') {
+        let features = [];
+        segmentEach(geom, (currentSegment) => {
+          features.push(currentSegment);
+        });
+
+        features.sort((a, b) => {
+          return pointToLineDistance(mousePoint, a) - pointToLineDistance(mousePoint, b);
+        });
+
+        const nearest = features[0];
+        const nearestPoint = nearestPointOnLine(nearest, mousePoint);
+        this.map.getSource('__interaction').setData(nearestPoint);
+
+      } else if (geom.type == 'LineString' || geom.type == 'MultiLineString') {
+        let features = [];
+        segmentEach(geom, (currentSegment) => {
+          features.push(currentSegment);
+        });
+
+        features.sort((a, b) => {
+          return pointToLineDistance(mousePoint, a) - pointToLineDistance(mousePoint, b);
+        });
+
+        const nearest = features[0];
+        const nearestPoint = nearestPointOnLine(nearest, mousePoint);
+        this.map.getSource('__interaction').setData(nearestPoint);
+
+      }
+    }
+  }
+
+  _mouseMoveEvent(e) {
+    if (this.snapEnabled) {
+      this._snap(e);
+      return;
+    }
+    this.mousePoint = point([e.lngLat.lng, e.lngLat.lat]);
   }
 
   _mouseMove() {
-    this.map.on('mousemove', (e) => {
+    this.map.on('mousemove', (e) => this._mouseMoveEvent(e));
+  }
 
-      if (this.isRunning) return;
-
-      var bbox = [
-        [e.point.x - this.tolerance * 3, e.point.y - this.tolerance * 3],
-        [e.point.x + this.tolerance * 3, e.point.y + this.tolerance * 3]
-      ];
-      const mousePoint = point([e.lngLat.lng, e.lngLat.lat]);
-      const features = this.map.queryRenderedFeatures(bbox);
-
-      if (!features.length) {
-        this.map.getSource('point').setData(this.getDataSourcePoint({ lat: 0, lng: 0 }));
-        return;
-      }
-
-      for (let i = 0; i < features.length; i++) {
-        const feature = features[i];
-        const geom = feature.geometry;
-
-        if (geom.type === 'Point') {
-          const dist = distance(geom, mousePoint) * 1000.0;
-          if (dist < this.tolerance) {
-            const coordinates = feature.geometry.coordinates.slice();
-            this.map.getSource('point').setData(this.getDataSourcePoint({ lat: coordinates[1], lng: coordinates[0] }));
-          } else {
-            this.map.getSource('point').setData(this.getDataSourcePoint({ lat: 0, lng: 0 }));
-          }
-
-        } else if (geom.type == 'MultiPolygon' || geom.type == 'Polygon') {
-          let features = [];
-          segmentEach(geom, (currentSegment) => {
-            features.push(currentSegment);
-          });
-
-          features.sort((a, b) => {
-            return pointToLineDistance(mousePoint, a) - pointToLineDistance(mousePoint, b);
-          });
-
-          const nearest = features[0];
-          // this.map.getSource('test').setData(nearest);
-
-          //nearestPointOnLine
-          const nearestPoint = nearestPointOnLine(nearest, mousePoint);
-          this.map.getSource('point').setData(nearestPoint);
-
-        } else if (geom.type == 'LineString' || geom.type == 'MultiLineString') {
-          let features = [];
-          segmentEach(geom, (currentSegment) => {
-            features.push(currentSegment);
-          });
-
-          features.sort((a, b) => {
-            return pointToLineDistance(mousePoint, a) - pointToLineDistance(mousePoint, b);
-          });
-
-          const nearest = features[0];
-          // this.map.getSource('test').setData(nearest);
-
-          //nearestPointOnLine
-          const nearestPoint = nearestPointOnLine(nearest, mousePoint);
-          this.map.getSource('point').setData(nearestPoint);
-
-        }
-
-
-
-
-      }
-
-
-    })
+  getEmptyDataSource() {
+    return {
+      'type': 'FeatureCollection',
+      'features': []
+    }
   }
 
   getDataSourcePoint({ lat, lng }) {
@@ -101,57 +131,6 @@ class Interaction {
 
 
   getPoint() {
-
-    const geojson = {
-      'type': 'FeatureCollection',
-      'features': [
-        {
-          'type': 'Feature',
-          'geometry': {
-            'type': 'Point',
-            'coordinates': [0, 0]
-          }
-        }
-      ]
-    };
-
-    this.map.addSource('point', {
-      'type': 'geojson',
-      'data': geojson
-    });
-
-    this.map.addLayer({
-      'id': 'point',
-      "type": "symbol",
-      'source': 'point',
-      "layout": {
-        "text-field": "×", // Unicode character for 'X'
-        "text-size": 24,
-      },
-      "paint": {
-        "text-color": "magenta"
-      }
-    });
-
-    this.map.addSource('test', {
-      'type': 'geojson',
-      'data': geojson
-    });
-
-    this.map.addLayer({
-      'id': 'test',
-      "type": "line",
-      'source': 'test',
-      "layout": {
-        "line-join": "round",
-        "line-cap": "round"
-      },
-      "paint": {
-        "line-color": "#ff0000", // Red lines
-        "line-width": 8,
-        "line-opacity": 0.8
-      }
-    });
 
     this._mouseMove();
   }
