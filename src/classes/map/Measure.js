1209ae010354acc4860f0e151fd19ca9fbbe65cd
Pedro Farias <pedropsfarias@gmail.com>
2024-05-08 23:20:56 -0300

    Many modifications

--- src/classes/map/Measure.js
+++ src/classes/map/Measure.js
@@ -1,11 +1,156 @@
-import Interaction from './Interaction.js'
+import Interaction from './Interaction.js';
+import MapSingleton from '../MapSingleton.js';
+import { featureEach, segmentEach, length } from '@turf/turf';
 
 class Measure {
   constructor() {
-
-    let interaction = new Interaction()
-    interaction.getPolygon()
+    this.interaction = new Interaction();
+    this.map = MapSingleton.getInstance().getMap();
+    this._createInternalLayers();
   }
+
+  _createInternalLayers() {
+    this.map.addSource('__measureDraw', {
+      'type': 'geojson',
+      'data': this._getEmptyDataSource()
+    });
+
+    this.map.addLayer({
+      'id': '__measureDrawLine',
+      'type': 'line',
+      'source': '__measureDraw',
+      'paint': {
+        'line-color': '#555',
+        'line-width': 2
+      },
+    });
+
+    this.map.addLayer({
+      'id': '__measureDrawPoint',
+      'type': 'circle',
+      'source': '__measureDraw',
+      'paint': {
+        'circle-radius': 3,
+        'circle-color': '#000',
+      },
+    });
+
+    //label 
+    this.map.addLayer({
+      'id': '__measureDrawLabel',
+      'type': 'symbol',
+      'source': '__measureDraw',
+      'layout': {
+        'text-field': ['get', 'label'],
+        'text-size': 12,
+        "text-anchor": "top-left",
+        "text-offset": [0.2, 0.2]
+      },
+      'filter': ['==', 'measureType', 'coords']
+    });
+  }
+
+  /**
+   * @private
+   * @memberof Interaction
+   * @description Get empty data source
+   * @returns {Object} Empty data source
+   */
+  _getEmptyDataSource() {
+    return {
+      'type': 'FeatureCollection',
+      'features': []
+    }
+  }
+
+  _gd2dms(coords) {
+
+    function toDMS(value) {
+      const degrees = Math.floor(Math.abs(value));
+      const minutesFloat = (Math.abs(value) - degrees) * 60;
+      const minutes = Math.floor(minutesFloat);
+      const seconds = ((minutesFloat - minutes) * 60).toFixed(2);
+
+      return `${degrees}° ${minutes}' ${seconds}"`;
+    }
+
+    const [longitude, latitude] = coords;
+
+    const longitudeDir = longitude < 0 ? 'W' : 'E';
+    const latitudeDir = latitude < 0 ? 'S' : 'N';
+
+    const longitudeDMS = toDMS(longitude) + longitudeDir;
+    const latitudeDMS = toDMS(latitude) + latitudeDir;
+
+    return `${longitudeDMS} ${latitudeDMS}`;
+  }
+
+  _updateSourceData(inputFeatures) {
+
+    let data = this.map.getSource('__measureDraw')._data;
+    let features = [];
+
+    segmentEach(data, (currentFeature) => {
+      features.push(currentFeature);
+    });
+
+    featureEach(inputFeatures, (currentFeature) => {
+      features.push(currentFeature);
+    });
+
+    this.map.getSource('__measureDraw').setData({
+      'type': 'FeatureCollection',
+      'features': features
+    });
+
+    console.log(this.map.getSource('__measureDraw')._data);
+  }
+
+  _getDistance(feature) {
+    const featureLength = length(feature);
+    feature.properties = {
+      'label': `${(featureLength * 1000.0).toFixed(2)} m`,
+      'measureType': 'distance'
+    };
+    return feature;
+  }
+
+  _measureDistances(features) {
+    const featuresDist = [];
+    segmentEach(features, (currentSegment) => {
+      featuresDist.push(this._getDistance(currentSegment));
+    });
+    return {
+      'type': 'FeatureCollection',
+      'features': featuresDist
+    };
+
+  }
+
+  async getCoordinates() {
+    let feature = await this.interaction.getPoint();
+    let coords = feature.geometry.coordinates;
+    let formattedCoords = this._gd2dms(coords);
+
+    feature.properties = {
+      'label': formattedCoords,
+      'measureType': 'coords'
+    };
+
+    this._updateSourceData(feature);
+
+    return feature;
+  }
+
+  async getDistance() {
+    let feature = await this.interaction.getLineString();
+    let features = this._getDistance(feature);
+
+    this._updateSourceData(features);
+
+    return feature;
+  }
+
 }
 
 export default Measure
--- src/components/map/MeasureComponent.vue
+++ src/components/map/MeasureComponent.vue
@@ -1,20 +1,49 @@
 <template>
   <RibbonGroupComponent title="Medidas" width="220px">
     <RibbonItemGroupComponent rows="1" cols="2">
-      <Button label="Pontos" icon="pi pi-bookmark" severity="secondary" outlined aria-label="Bookmark" class="w-100" />
+      <Button
+        @click="getCoordinates"
+        label="Pontos"
+        icon="pi pi-bookmark"
+        severity="secondary"
+        outlined
+        aria-label="Bookmark"
+        class="w-100"
+      />
     </RibbonItemGroupComponent>
 
     <RibbonItemGroupComponent rows="1" cols="2">
-      <Button label="Distância" icon="pi pi-bookmark" severity="secondary" outlined aria-label="Bookmark"
-        class="w-100" />
+      <Button
+        @click="getDistance"
+        label="Distância"
+        icon="pi pi-bookmark"
+        severity="secondary"
+        outlined
+        aria-label="Bookmark"
+        class="w-100"
+      />
     </RibbonItemGroupComponent>
 
     <RibbonItemGroupComponent rows="1" cols="2">
-      <Button label="Áreas" icon="pi pi-bookmark" severity="secondary" outlined aria-label="Bookmark" class="w-100" />
+      <Button
+        label="Áreas"
+        icon="pi pi-bookmark"
+        severity="secondary"
+        outlined
+        aria-label="Bookmark"
+        class="w-100"
+      />
     </RibbonItemGroupComponent>
 
     <RibbonItemGroupComponent rows="1" cols="2">
-      <Button label="Ângulos" icon="pi pi-bookmark" severity="secondary" outlined aria-label="Bookmark" class="w-100" />
+      <Button
+        label="Ângulos"
+        icon="pi pi-bookmark"
+        severity="secondary"
+        outlined
+        aria-label="Bookmark"
+        class="w-100"
+      />
     </RibbonItemGroupComponent>
 
     <!-- <RibbonItemGroupComponent title="Testes" rows="1" cols="4">
@@ -25,26 +54,25 @@
 </template>
 
 <script>
-import Measure from '@/classes/map/Measure.js';
+import Measure from '@/classes/map/Measure.js'
 
 export default {
   name: 'MeasureComponent',
   data() {
     return {
-      message: 'Hello from MeasureComponent'
-    };
+      measure: new Measure()
+    }
   },
-  mounted() {
-    //console.log('MeasureComponent mounted', this.app.map  );
-    new Measure(this.app.map);
-    // measure.init();
-
-
-
-    //this.app.map.addControl(new Measure());
-  },
-  methods: {}
-};
+  mounted() {},
+  methods: {
+    getCoordinates() {
+      console.log(this.measure.getCoordinates())
+    },
+    getDistance() {
+      console.log(this.measure.getDistance())
+    }
+  }
+}
 </script>
 
 <style scoped></style>
