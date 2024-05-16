b503831661d1f16216af24aa94b0c0ecc311c2ed
Pedro Farias <pedropsfarias@gmail.com>
2024-05-16 18:16:37 -0300

    Navigation Implementation

--- src/components/map/NavigationComponent.vue
+++ src/components/map/NavigationComponent.vue
@@ -1,118 +1,39 @@
 <template>
-  <RibbonGroupComponent title="Navegação" width="220px">
-    <RibbonItemGroupComponent rows="2" cols="2">
+  <RibbonGroupComponent title="Navegação" width="180px">
+    <RibbonItemGroupComponent rows="2" cols="1">
       <Button
-        @click="getCoordinates"
-        label="Pontos"
-        icon="fg-lg fg fg-poi-home-o"
+        @click="goToHome"
+        v-tooltip="'Ir para a posição inicial'"
+        icon="fg-2x fg fg-poi-home-o"
         severity="secondary"
         outlined
         aria-label="Bookmark"
         class="w-100 h-100"
-        :disabled="activeTool"
+        style="height: 100% !important"
       />
     </RibbonItemGroupComponent>
   </RibbonGroupComponent>
-
-  <Dialog v-model:visible="resumeVisible" header="Medidas" :style="{ width: '25rem' }">
-    <div v-if="resume.length == 0">Nenhuma medida realizada</div>
-    <div v-for="(r, i) in resume" :key="i">
-      <div>
-        <b>{{ r.title }}</b> <br />
-        {{ r.items }}<br />
-        <div v-if="r.total">Total: {{ r.total }}</div>
-      </div>
-      <Divider />
-    </div>
-    <Button
-      v-if="resume.length > 0"
-      @click="clear"
-      label="Limpar"
-      icon="pi pi-eraser"
-      severity="secondary"
-      outlined
-    />
-  </Dialog>
 </template>
 
 <script>
+import MapSingleton from '@/classes/MapSingleton.js';
 export default {
   name: 'NavigationComponent',
   data() {
     return {
-      resumeVisible: false,
-      measure: null,
-      measures: [],
-      resume: [],
-      activeTool: null
+      map: MapSingleton.getInstance().getMap()
     };
   },
   mounted() {},
   methods: {
-    async getCoordinates() {
-      this.activeTool = 'coords';
-      this.measures.push(await this.measure.getCoordinates());
-      this.buildResume();
-      this.activeTool = null;
-    },
-    async getDistance() {
-      this.activeTool = 'distance';
-      this.measures.push(await this.measure.getDistance());
-      this.buildResume();
-      this.activeTool = null;
-    },
-    async getArea() {
-      this.activeTool = 'area';
-      this.measures.push(await this.measure.getArea());
-      this.buildResume();
-      this.activeTool = null;
-    },
-    async getAzimuth() {
-      this.activeTool = 'azimuth';
-      this.measures.push(await this.measure.getAzimuth());
-      this.buildResume();
-      this.activeTool = null;
-    },
-    clear() {
-      this.measure.clear();
-      this.measures = [];
-      this.resume = [];
-    },
-    buildResume() {
-      this.resume = [];
-      for (let i = 0; i < this.measures.length; i++) {
-        if (this.measures[i].type == 'FeatureCollection') {
-          const props = [];
-          for (let j = 0; j < this.measures[i].features.length; j++) {
-            props.push(this.measures[i].features[j].properties);
-          }
-          this.resume.push({
-            title: this._getTitle(this.measures[i].features[0].properties),
-            items: props.map((p) => p.label).join(', '),
-            total: this.measures[i].features[0].properties.total
-          });
-        } else {
-          this.resume.push({
-            title: this._getTitle(this.measures[i].properties),
-            items: this.measures[i].properties.label
-          });
-        }
-      }
-    },
-    showResume() {
-      this.resumeVisible = true;
-    },
-    _getTitle(measure) {
-      switch (measure.measureType) {
-        case 'coords':
-          return 'Coordenadas';
-        case 'distance':
-          return 'Distância';
-        case 'area':
-          return 'Área';
-        case 'azimuth':
-          return 'Azimute';
-      }
+    goToHome() {
+      const initialCenter = this.map.getStyle().center;
+      const initialZoom = this.map.getStyle().zoom;
+      this.map.flyTo({
+        center: initialCenter,
+        zoom: initialZoom,
+        speed: 0.5
+      });
     }
   }
 };
